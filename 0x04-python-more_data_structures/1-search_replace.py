f4d1b6d18ea172d49d36b02f04bc21efd4498669
karima <karima.mtq@gmail.com>
2024-01-11 07:04:43 -0800

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = list(my_list)
-        while search in new_list:
-            new_list.remove(search)
-        return new_list
+    new_list = list(my_list)
+    while search in new_list:
+        new_list.remove(search)
+    return new_list
