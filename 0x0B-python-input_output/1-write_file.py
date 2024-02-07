8620ef12a64109a9680c272d83bbbffe28699d77
karima <karima.mtq@gmail.com>
2024-02-07 14:36:20 -0800

    ex

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-write file
+write file mod
 """
 
-def write_file(filename=""):
-    """ write file"""
+def write_file(filename="", text=""):
+    """def write file"""
     with open(filename, "w", encoding="UTF-8") as f:
-        print(f.read(), end="")
+        return f.write(text)
